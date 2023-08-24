5e8a9492ec8bbbfb1a81c4e0ac7afe9967ea0b61
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-08-24 19:59:37 -0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HealthModule} from "./domains/health/health.module";
+import {AuthModule} from "./auth/auth.module";
+import {ConfigModule} from "@nestjs/config";
+import {PrismaModule} from "./prisma/prisma.module";
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HealthModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
