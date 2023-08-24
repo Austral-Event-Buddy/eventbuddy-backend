4f9aa0cd58932d46c8a4b2bf38102c8365f764d0
cataghiglione <catalina.ghiglione@ing.austral.edu.ar>
2023-08-24 19:46:26 -0300

    Event entity and guest entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HealthModule} from "./domains/health/health.module";
+import { EventModule } from './domains/event/event.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
