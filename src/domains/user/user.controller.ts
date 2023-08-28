d9ac76e680a502ed54b09085970f5165e7848027
MauroKinderknecht <56045994+MauroKinderknecht@users.noreply.github.com>
2023-08-28 03:48:26 +0000

    Deployment (#11)
    Co-authored-by: Mauro Kinderknecht <mauro.mkinderknecht@gmail.com>

--- src/domains/user/user.controller.ts
+++ src/domains/user/user.controller.ts
@@ -1,17 +1,14 @@
-import {Controller, Get, Request, UseGuards} from "@nestjs/common";
-import {UserService} from "./user.service";
-import {JwtAuthGuard} from "../auth/auth.guard";
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/auth.guard';
 import { Request as ExpressRequest } from 'express';
 
 @Controller('user')
-export class UserController{
-
-    constructor(private userService: UserService) {
-    }
-    @UseGuards(JwtAuthGuard)
-    @Get('me')
-    getMe(@Request() request: ExpressRequest) {
-        delete request.user['password'];
-        return request.user;
-    }
+export class UserController {
+  constructor() {}
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Request() request: ExpressRequest) {
+    delete request.user['password'];
+    return request.user;
+  }
 }
--- src/domains/user/user.module.ts
+++ src/domains/user/user.module.ts
@@ -1,15 +1,7 @@
-import {Module} from "@nestjs/common";
-import {UserController} from "./user.controller";
-import {UserService} from "./user.service";
-import {UserRepository} from "./user.repository";
-import {PrismaService} from "../../prisma/prisma.service";
-import {ConfigService} from "@nestjs/config";
-import {JwtService} from "@nestjs/jwt";
-import {AuthService} from "../auth/auth.service";
-import {AuthRepository} from "../auth/auth.repository";
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
 @Module({
-    controllers: [UserController],
-    providers:[UserService, UserRepository, PrismaService, ConfigService, AuthService, AuthRepository, JwtService],
+  controllers: [UserController],
 })
 export class UserModule {}
