e8d58c3cf353051a6cfc3c2da64f52e454a40ed2
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-09-09 09:53:16 -0300

    merge

---
+++ prisma/migrations/20230831140631_/migration.sql
@@ -0,0 +1,36 @@
+-- CreateEnum
+CREATE TYPE "confirmationStatus" AS ENUM ('PENDING', 'ATTENDING', 'NOT_ATTENDING', 'HOST');
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "creatorId" INTEGER NOT NULL,
+    "coordinates" DOUBLE PRECISION[],
+    "confirmationDeadline" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Guest" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "eventId" INTEGER NOT NULL,
+    "confirmationStatus" "confirmationStatus" NOT NULL,
+
+    CONSTRAINT "Guest_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guest" ADD CONSTRAINT "Guest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guest" ADD CONSTRAINT "Guest_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/domains/event/repository/event.repository.ts
+++ src/domains/event/repository/event.repository.ts
@@ -1,8 +1,9 @@
 import { PrismaService } from '../../../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { NewEventInput } from '../input';
-import { updateEventInput } from '../input/updateEvent.input';
+import { updateEventInput } from '../input';
 import { IEventRepository } from './event.repository.interface';
+import { confirmationStatus } from '@prisma/client';
 
 @Injectable()
 export class EventRepository implements IEventRepository {
@@ -11,13 +12,54 @@ export class EventRepository implements IEventRepository {
   async getEventsByUserId(userId: number) {
     return this.prisma.event.findMany({
       where: {
-          OR: [{creatorId: userId}, {guests: {some: {userId: userId}}}],
-      }
-
+        OR: [{ creatorId: userId }, { guests: { some: { userId: userId } } }],
+      },
+      // },
+      // select: {
+      //   id: true,
+      //   name: true,
+      //   description: true,
+      //   coordinates: true,
+      //   date: true,
+      //   confirmationDeadline: true,
+      // },
     });
   }
 
-
+  // async getEventsByUserId(userId: number) {
+  //     return this.prisma.event.findMany({
+  //         where: {
+  //             OR: [
+  //                 {creatorId: userId},
+  //                 {guests: {some: {userId: userId}}}
+  //             ]
+  //         },
+  //         select: {
+  //             name: true,
+  //             description: true,
+  //             coordinates: true,
+  //             date: true,
+  //             guests: {
+  //                 where: {
+  //                     userId: userId
+  //                 },
+  //                 select: {
+  //                     confirmationStatus: true
+  //                 }
+  //             },
+  //             _count: {
+  //                 select: {
+  //                     guests: {
+  //                         where: {
+  //                             confirmationStatus: {in: ["ATTENDING", "HOST"]}
+  //                         }
+  //                     }
+  //                 }
+  //             }
+  //
+  //         }
+  //     });
+  // }
   async countGuestsByEventId(eventId: number) {
     return this.prisma.guest.count({
       where: {
@@ -27,15 +69,20 @@ export class EventRepository implements IEventRepository {
     });
   }
 
-  async findConfirmationStatus(userId: number, eventId: number) {
-    return this.prisma.guest.findUnique({
-      where: {
-        userId_eventId: { userId, eventId },
-      },
-      select: {
-        confirmationStatus: true,
-      },
-    });
+  async findConfirmationStatus(
+    userId: number,
+    eventId: number,
+  ): Promise<confirmationStatus> {
+    return (
+      await this.prisma.guest.findUnique({
+        where: {
+          userId_eventId: { userId, eventId },
+        },
+        select: {
+          confirmationStatus: true,
+        },
+      })
+    ).confirmationStatus;
   }
 
   async getEventsByNameOrDescriptionAndUserId(userId: number, input: string) {
@@ -58,7 +105,7 @@ export class EventRepository implements IEventRepository {
             ],
           },
         ],
-      }
+      },
       // select: {
       //   id: true,
       //   name: true,
@@ -117,9 +164,7 @@ export class EventRepository implements IEventRepository {
     return this.prisma.event.findUnique({
       where: {
         id: eventId,
-      },
-      select: {
-        creatorId: true,
+        creatorId: userId
       },
     });
   }
@@ -141,5 +186,59 @@ export class EventRepository implements IEventRepository {
     } catch (error) {
       throw new Error(`Error when deleting event: ${error}`);
     }
+  }
+
+  getEvent(eventId: number) {
+    return this.prisma.event.findUnique({
+      where: {
+        id: eventId,
+      },
+    });
+  }
+
+  async inviteGuest(eventId: number, invitedId: number) {
+    return this.prisma.guest.create({
+      data: {
+        userId: invitedId,
+        eventId: eventId,
+        confirmationStatus: 'PENDING',
+      },
+    });
+  }
+
+  async answerInvite(guestId: number, answer: confirmationStatus) {
+    return this.prisma.guest.update({
+      where: {
+        id: guestId,
+      },
+      data: {
+        confirmationStatus: answer,
+      },
+    });
+  }
+
+  //Should only return with status pending (?)
+  async getInvitesByUser(userId: number) {
+    return this.prisma.guest.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  getGuest(guestId: number) {
+    return this.prisma.guest.findUnique({
+      where: {
+        id: guestId,
+      },
+    });
+  }
+
+  getGuestsByEvent(eventId: number) {
+    return this.prisma.guest.findMany({
+      where: {
+        eventId: eventId,
+      },
+    });
   }
 }
