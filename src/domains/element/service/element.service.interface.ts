25e659057e37de37cdfb011cd1e97ce069dcd565
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-10-22 16:54:37 -0300

    element service

--- src/domains/element/service/element.service.interface.ts
+++ src/domains/element/service/element.service.interface.ts
@@ -1,5 +1,21 @@
-import {NewElementInput} from "../input/newElementInput";
+import {ElementDto} from "../dto/element.dto";
+import {
+	ElementInput,
+	NewElementInput,
+	UpdateElementInput,
+	UserElementInput
+} from "../input";
 
 export abstract class IElementService{
 	abstract createElement(input: NewElementInput): Promise<ElementDto>;
+
+	abstract addUser(userId: number, input: UserElementInput): Promise<ElementDto>;
+
+	abstract deleteUser(userId: number, input: UserElementInput): Promise<ElementDto>;
+
+	abstract updateElement(input: UpdateElementInput) : Promise<ElementDto>;
+
+	abstract deleteElement(input: ElementInput);
+
+	abstract getElementById(input: ElementInput): Promise<ElementDto>;
 }
--- src/domains/element/service/element.service.ts
+++ src/domains/element/service/element.service.ts
@@ -1,7 +1,14 @@
-import {Injectable} from "@nestjs/common";
+import {ForbiddenException, Injectable} from "@nestjs/common";
 import {IElementService} from "./element.service.interface";
-import {NewElementInput} from "../input/newElementInput";
+import {NewElementInput} from "../input/newElement.input";
 import {IElementRepository} from "../repository/element.repository.interface";
+import {UserElementInput} from "../input/userElement.input";
+import {UserDto} from "../../user/dto/user.dto";
+import {ExtractJwt} from "passport-jwt";
+import fromAuthHeaderAsBearerToken = ExtractJwt.fromAuthHeaderAsBearerToken;
+import {ElementDto} from "../dto/element.dto";
+import {ElementInput, UpdateElementInput} from "../input";
+import {MaxUsersDto} from "../dto/maxUsersDto";
 
 @Injectable()
 export class ElementService implements IElementService {
@@ -10,6 +17,54 @@ export class ElementService implements IElementService {
 
 	createElement(input: NewElementInput): Promise<ElementDto> {
 		return this.repository.createElement(input);
+	}
+
+	async addUser(userId: number, input: UserElementInput) {
+		const users = await this.repository.getUsers(input.elementId);
+		if(!this.checkUserInUsers(users, userId) && await this.checkUsersInElement(users.length, input.elementId)){
+			const element = await this.repository.addUser(userId, input.elementId);
+			if(element === null) throw new ForbiddenException("The element id is not vaid");
+			return element;
+		}
+		throw new ForbiddenException("User is already in charge of this element");
+	}
+
+	async deleteUser(userId: number, input: UserElementInput) {
+		const users = await this.repository.getUsers(input.elementId);
+		if(this.checkUserInUsers(users, userId)){
+			const element = await this.repository.deleteUser(userId, input.elementId);
+			if(element === null) throw new ForbiddenException("The element id is not vaid");
+			return element;
+		}
+		throw new ForbiddenException("User wasn't in charge of this element");
+	}
+
+	async updateElement(input: UpdateElementInput): Promise<ElementDto> {
+		const element = await this.repository.updateElement(input);
+		if(element === null) throw new ForbiddenException("The element id is not vaid");
+		return element;
+	}
+
+	async deleteElement(input: ElementInput) {
+		this.repository.deleteElement(input.id);
+	}
+
+	async getElementById(input: ElementInput): Promise<ElementDto> {
+		const element = await this.repository.getElementById(input.id);
+		if(element === null) throw new ForbiddenException("The element id is not vaid");
+		return element;
+	}
+
+	private checkUserInUsers(users: UserDto[], userId: number) {
+		users.forEach(user => {
+			if(user.id === userId) { return true }
+		});
+		return false
+	}
+
+	private async checkUsersInElement(usersLen: number, elementId: number) {
+		const maxUsers = await this.repository.getMaxUsers(elementId);
+		return usersLen < maxUsers.maxUsers;
 	}
 
 }
