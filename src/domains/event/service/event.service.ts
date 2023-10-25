4508ebde8041f8def8d8d75fd259192be4c2378f
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-10-25 12:17:23 -0300

    fix event service

--- src/domains/event/service/event.service.ts
+++ src/domains/event/service/event.service.ts
@@ -1,24 +1,14 @@
 import {
-    ForbiddenException,
-    Injectable,
-    NotFoundException,
-    UnauthorizedException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import {
   answerInviteInput,
   getEventsBySearchInput,
   inviteGuestInput,
-  NewEventInput, updateEventInput,
-} from '../input';
-import { IEventService } from './event.service.interface';
-import {Event} from '@prisma/client';
-import { IEventRepository } from "../repository";
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { EventInfoOutputDto } from '../dto/event.info.output.dto';
-    answerInviteInput,
-    getEventsBySearchInput,
-    inviteGuestInput,
-    NewEventInput,
+  NewEventInput,
 } from '../input';
 import {IEventService} from './event.service.interface';
 import {updateEventInput} from '../input';
@@ -31,147 +21,96 @@ import {ElementDto} from "../../element/dto/element.dto";
 
 @Injectable()
 export class EventService implements IEventService {
-    constructor(private repository: IEventRepository) {
-    }
+  constructor(private repository: IEventRepository) {
+  }
 
-    async getEventsByUserId(userId: number) {
-        const events = await this.checkEvents(await this.repository.getEventsByUserId(userId), userId);
-        if (!events) {
-            throw new NotFoundException('No events found');
-        }
-        return this.toEventInfoOutput(events, userId);
+  async getEventsByUserId(userId: number) {
+    const events = await this.checkEvents(await this.repository.getEventsByUserId(userId), userId);
+    if (!events) {
+      throw new NotFoundException('No events found');
     }
+    return this.toEventInfoOutput(events, userId);
+  }
 
-    async getEventsByNameOrDescriptionAndUserId(
-        userId: number,
-        input: getEventsBySearchInput,
-    ) {
-        const events = await this.repository.getEventsByNameOrDescriptionAndUserId(
-            userId,
-            input.search,
-        );
-        if (!events) {
-            throw new NotFoundException('No events found');
-        }
-        const finalEvents = await this.checkEvents(events, userId);
-        return this.toEventInfoOutput(finalEvents, userId);
+  async getEventsByNameOrDescriptionAndUserId(
+      userId: number,
+      input: getEventsBySearchInput,
+  ) {
+    const events = await this.repository.getEventsByNameOrDescriptionAndUserId(
+        userId,
+        input.search,
+    );
+    if (!events) {
+      throw new NotFoundException('No events found');
     }
+    const finalEvents = await this.checkEvents(events, userId);
+    return this.toEventInfoOutput(finalEvents, userId);
+  }
 
-    async getEventByEventId(userId: number, eventId: number) {
-        const guest = await this.repository.getGuest(userId, eventId)
-        if (guest !== undefined) {
-            if (guest.confirmationStatus !== "NOT_ATTENDING") {
-                return await this.repository.getEvent(eventId)
-            }
-        } else throw new UnauthorizedException("User is not allowed to check this event information")
+  async getEventByEventId(userId: number, eventId: number) {
+    const guest = await this.repository.getGuest(userId, eventId)
+    if (guest !== undefined) {
+      if (guest.confirmationStatus !== "NOT_ATTENDING") {
+        return await this.repository.getEvent(eventId)
+      }
+    } else throw new UnauthorizedException("User is not allowed to check this event information")
+  }
+
+  async createEvent(userId: number, input: NewEventInput) {
+    return this.repository.createEvent(userId, input);
+  }
+
+  async checkGuestStatusOnEvent(userId: number, eventId: number) {
+    const event = await this.repository.getHostGuest(userId, eventId);
+    if (event === null) {
+      throw new ForbiddenException('User is not hosting this event');
+    } else return true;
+  }
+
+  async updateEvent(eventId: number, input: updateEventInput): Promise<EventDto> {
+    const event = await this.repository.updateEvent(eventId, input);
+    if (event === null) {
+      throw new NotFoundException('Event not found');
     }
-
-    async createEvent(userId: number, input: NewEventInput) {
-        return this.repository.createEvent(userId, input);
+    return {
+      id: event.id,
+      name: event.name,
+      description: event.description,
+      creatorId: event.creatorId,
+      coordinates: event.coordinates,
+      confirmationDeadline: event.confirmationDeadline,
+      date: event.date,
+      updatedAt: event.updatedAt,
+      createdAt: event.createdAt
     }
+  }
 
-    async checkGuestStatusOnEvent(userId: number, eventId: number) {
-        const event = await this.repository.getHostGuest(userId, eventId);
-        if (event === null) {
-            throw new ForbiddenException('User is not hosting this event');
-        } else return true;
+  async deleteEvent(userId: number, eventId: number) {
+    const event = await this.repository.checkIfUserIsCreator(userId, eventId);
+    if (event === null) {
+      throw new UnauthorizedException('User is not authorized to delete event');
     }
+    await this.repository.deleteEventAndGuests(eventId);
+    return true;
+  }
 
-    async updateEvent(eventId: number, input: updateEventInput): Promise<EventDto> {
-        const event = await this.repository.updateEvent(eventId, input);
-        if (event === null) {
-            throw new NotFoundException('Event not found');
-        }
-        return {
-            id: event.id,
-            name: event.name,
-            description: event.description,
-            creatorId: event.creatorId,
-            coordinates: event.coordinates,
-            confirmationDeadline: event.confirmationDeadline,
-            date: event.date,
-            updatedAt: event.updatedAt,
-            createdAt: event.createdAt
-        }
-    }
-
-    async deleteEvent(userId: number, eventId: number) {
-        const event = await this.repository.checkIfUserIsCreator(userId, eventId);
-        if (event === null) {
-            throw new UnauthorizedException('User is not authorized to delete event');
-        }
-        await this.repository.deleteEventAndGuests(eventId);
-        return true;
-    }
-
-    async inviteGuest(input: inviteGuestInput, userId: number) {
-        const eventId = input.eventId;
-        const invitedId = input.userId;
-        const hostGuest = await this.repository.getHostGuest(userId, eventId);
-        const event = await this.repository.getEvent(eventId);
-        if (hostGuest != null || event.creatorId === userId) {
-            if (!this.checkEventDate(event.date)) throw new ForbiddenException("The event date has passed")
-            else if (!this.checkEventDate(event.confirmationDeadline)) throw new ForbiddenException("The confirmation deadline has passed")
-            try {
-                return await this.repository.inviteGuest(eventId, invitedId, input.isHost);
-            } catch (error) {
-                if (error instanceof PrismaClientKnownRequestError) {
-                    if (error.code === 'P2002') {
-                        throw new ForbiddenException(
-                            'The user is already invited to this event',
-                        );
-                    }
-                }
-            }
-        } else {
-            throw new ForbiddenException('You are not a host of this event');
-        }
-    }
-
-    async answerInvite(input: answerInviteInput, userId: number) {
-        const eventId = input.eventId;
-        const guest = await this.repository.getGuest(userId, eventId);
-        const event = await this.repository.getEvent(eventId);
-        if (!this.checkEventDate(event.date))
-            throw new ForbiddenException("The confirmation deadline has passed");
-        else if (!await this.checkConfirmationDeadline(event.confirmationDeadline, guest.userId, event.id))
-            throw new ForbiddenException("The confirmation deadline has passed");
-        if (guest['userId'] == userId) {
-            return await this.repository.answerInvite(guest.id, input.answer);
-        } else {
-            throw new ForbiddenException('This invite is not yours');
-        }
-    }
-
-    async getInvitesByUser(userId: number) {
-        return this.repository.getInvitesByUser(userId);
-    }
-
-    getGuestsByEvent(eventId: number) {
-        return this.repository.getGuestsByEvent(eventId);
-    }
-
-    private async toEventInfoOutput(
-        events: Event[],
-        userId: number,
-    ): Promise<EventInfoOutputDto[]> {
-        let eventInfoOutput: EventInfoOutputDto[] = [];
-        for (const event of events) {
-            const confirmationStatus = await this.repository.findConfirmationStatus(
-                userId,
-                event.id,
+  async inviteGuest(input: inviteGuestInput, userId: number) {
+    const eventId = input.eventId;
+    const invitedId = input.userId;
+    const hostGuest = await this.repository.getHostGuest(userId, eventId);
+    const event = await this.repository.getEvent(eventId);
+    if (hostGuest != null || event.creatorId === userId) {
+      if (!this.checkEventDate(event.date)) throw new ForbiddenException("The event date has passed")
+      else if (!this.checkEventDate(event.confirmationDeadline)) throw new ForbiddenException("The confirmation deadline has passed")
+      try {
+        return await this.repository.inviteGuest(eventId, invitedId, input.isHost);
+      } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === 'P2002') {
+            throw new ForbiddenException(
+                'The user is already invited to this event',
             );
-            const guestCount = await this.repository.countGuestsByEventId(event.id);
-            eventInfoOutput.push({
-                id: event.id,
-                name: event.name,
-                description: event.description,
-                coordinates: event.coordinates,
-                date: event.date,
-                confirmationDeadline: event.confirmationDeadline,
-                confirmationStatus: confirmationStatus,
-                guests: guestCount,
-            });
+          }
         }
       }
     } else {
@@ -181,7 +120,7 @@ export class EventService implements IEventService {
 
   async answerInvite(input: answerInviteInput, userId: number) {
     const eventId = input.eventId;
-    const guest = await this.repository.getGuest(userId,eventId);
+    const guest = await this.repository.getGuest(userId, eventId);
     const event = await this.repository.getEvent(eventId);
     if (!this.checkEventDate(event.date))
       throw new ForbiddenException("The confirmation deadline has passed");
@@ -237,7 +176,7 @@ export class EventService implements IEventService {
   }
 
   private async checkEvents(events: Event[], userId: number): Promise<Event[]> {
-    const result : Event[] = [];
+    const result: Event[] = [];
     for (const event of events)
       if (this.checkEventDate(event.date) && await this.checkConfirmationDeadline(event.confirmationDeadline, userId, event.id))
         result.push(event);
